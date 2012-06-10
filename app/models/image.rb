04dfae60debe3d705a78939edc2bb9e2e972f6ac
defsan <defsan@gmail.com>
2012-06-10 12:36:49 -0700

    Git commit design

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,11 +3,10 @@ class Image < ActiveRecord::Base
   belongs_to :idea
 
   has_attached_file :image,
-  	  :path => ":rails_root/public/photos/:idea_id/:id_:style",
-      :url => "/photos/:idea_id/:id_:style",
+  	  :path => "/photos/:idea_id/:id_:style",
   	:styles => {
-  		:thumb=> "280x187",
-  		:small  => "50x50" },
+  		:thumb => "280x187",
+  		:med => "620x245#" },
     :storage => :s3,
     :bucket => 'joinme',
     :s3_credentials => {
