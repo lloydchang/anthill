ae51b223a5a2d8bb5a627032236cf71d157f146d
defsan <defsan@gmail.com>
2012-06-11 22:25:00 -0700

    Adding default email

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -3,7 +3,9 @@ ActionMailer::Base.smtp_settings = {
   :address        => ENV['MAILGUN_SMTP_SERVER'],
   :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
   :password       => ENV['MAILGUN_SMTP_PASSWORD'],
-  :domain         => 'yourapp.heroku.com',
+  :domain         => 'getanthill.com',
   :authentication => :plain,
 }
 ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.default :from => 'support@getanthill.com'
